64ed45f4c97d56066ef9d5427d6a40de145f4d5e
Isaac Levy <isaa0267@stud.kea.dk>
2023-06-15 20:38:52 +0200

    The javascript code.

---
+++ Profile/index.js
@@ -0,0 +1,9 @@
+favouritMeovieGenre("military")
+favouritfrit("banana")
+favouritMode("light")
+favouritEdgeStyle("sharp")
+
+// The first function. 
+function setProp(prop,value){
+  document.documentElement.style.setProperty(prop,value)  
+}
